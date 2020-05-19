19bc2848f17870d519124d79c83ec302a1b22ee4
Zulkarnain Mohamed <zulkarnain.md.ismail@gmail.com>
2020-05-19 17:06:09 +0200

    Rails Task Manager until create

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,2 +1,19 @@
 class TasksController < ApplicationController
+  def index
+    # See all task, retrieve in variable to store
+    @tasks = Task.all
+  end
+
+  def show
+    @task = Task.find(params[:id])
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(params[:task])
+    @task.save
+  end
 end
