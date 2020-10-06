7d3cd8335d08ce4d711f99c3668da93b4ab4ba9a
Zulkarnain Mohd <zulkarnain.md.ismail@gmail.com>
2020-10-06 19:17:11 +0200

    rails new

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,3 @@
 Rails.application.routes.draw do
-  # See all tasks
-  get 'tasks', to: 'tasks#index'
-  # Create a task
-  get 'tasks/new', to: 'tasks#new'
-  post 'tasks', to: 'tasks#create'
-  # See details about one task
-  get 'tasks/:id', to: 'tasks#show', as: :task
-  # Update a task
-  # Destroy a task
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # get 'list', to: 'tasks#list'
 end
